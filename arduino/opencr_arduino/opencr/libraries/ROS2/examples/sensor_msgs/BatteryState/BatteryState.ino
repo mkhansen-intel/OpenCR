f274a5b98b5821aa293194180ca53da49012a645
Kei <kkw@robotis.com>
2018-07-20 21:11:42 +0900

    Applied new API.

--- arduino/opencr_arduino/opencr/libraries/ROS2/examples/sensor_msgs/BatteryState/BatteryState.ino
+++ arduino/opencr_arduino/opencr/libraries/ROS2/examples/sensor_msgs/BatteryState/BatteryState.ino
@@ -3,9 +3,14 @@
 #include "sensor_msgs/BatteryState.hpp"
 
 
-#define DEBUG_SERIAL Serial2  
-#define RTPS_SERIAL  Serial   //OpenCR USB
+#define DEBUG_SERIAL SerialBT2
+#define RTPS_SERIAL  Serial    //OpenCR USB
 
+#define BATTERY_STATE_PUBLISH_FREQUENCY 2 //hz
+
+
+void publishBatteryState(void* topic_msg);
+void subscribeBatteryState(void* topic_msg);
 
 
 class BatteryStatePubSub : public ros2::Node
@@ -14,108 +19,24 @@ public:
   BatteryStatePubSub()
   : Node()
   {
-    memset(cell_voltage_data_, 0, sizeof(cell_voltage_data_));
-
+    DEBUG_SERIAL.println();
     publisher_ = this->createPublisher<sensor_msgs::BatteryState>("BatteryState");
-    publisher_->setPublishInterval(2); // 2 hz
-    subscriber_ = this->createSubscriber<sensor_msgs::BatteryState>("BatteryState");
-    subscriber_->subscribe(STREAMID_BUILTIN_RELIABLE);
+    this->createWallFreq(BATTERY_STATE_PUBLISH_FREQUENCY, publishBatteryState, publisher_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /BatteryState : "); DEBUG_SERIAL.println((publisher_!=NULL?"Success":"Fail"));
+    subscriber_ = this->createSubscriber<sensor_msgs::BatteryState>("BatteryState", subscribeBatteryState);
+    DEBUG_SERIAL.print(" [Subscriber Create]  /BatteryState : "); DEBUG_SERIAL.println((subscriber_!=NULL?"Success":"Fail"));
   }
 
 
 private:
-
-  void timerCallback()
-  {
-    if(publisher_->isTimeToPublish())
-    {
-      nano_time_ = ros2::getNanoTime();
-
-      sensor_msgs::BatteryState battery_state_topic;
-      battery_state_topic.header.frame_id = (char*) "OpenCR BatteryState";
-      battery_state_topic.header.stamp.sec = (int32_t)(nano_time_/(uint64_t)1000000000UL);
-      battery_state_topic.header.stamp.nanosec = (uint32_t)(nano_time_%(uint64_t)1000000000UL);
-
-      battery_state_topic.voltage = 1;
-      battery_state_topic.current = 2;
-      battery_state_topic.charge = 3;
-      battery_state_topic.capacity = 4;
-      battery_state_topic.design_capacity = 5;
-      battery_state_topic.percentage = 6;
-
-      battery_state_topic.power_supply_status = 0;
-      battery_state_topic.power_supply_health = 1;
-      battery_state_topic.power_supply_technology = 2;
-
-      battery_state_topic.present = true;
-      battery_state_topic.cell_voltage = cell_voltage_data_;
-      battery_state_topic.cell_voltage_size = sizeof(cell_voltage_data_)/sizeof(float);
-      for(uint32_t i = 0; i < battery_state_topic.cell_voltage_size; i++)
-      {
-        battery_state_topic.cell_voltage[i] = 3.7;
-      }
-
-      battery_state_topic.location = (char*)"Seoul";
-      battery_state_topic.serial_number = (char*)"123-456-789";
-
-      if(publisher_ != NULL)
-      {
-        publisher_->publish(&battery_state_topic, STREAMID_BUILTIN_RELIABLE);
-      }
-    }
-  }
-
-  void userTopicCallback(uint8_t topic_id, void* topic_msg)
-  {
-    if(topic_id == subscriber_->topic_id_)
-    {
-      sensor_msgs::BatteryState* topic = (sensor_msgs::BatteryState*)topic_msg;
-
-      DEBUG_SERIAL.println();
-      DEBUG_SERIAL.print(" Header(frameID,sec,nanosec): "); 
-      DEBUG_SERIAL.print(topic->header.frame_id); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.print(topic->header.stamp.sec); DEBUG_SERIAL.print("."); 
-      DEBUG_SERIAL.println(topic->header.stamp.nanosec);
-      DEBUG_SERIAL.print(" Value(voltage,current,charge,capacity,design capacity,percentage): ");
-      DEBUG_SERIAL.print(topic->voltage); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.print(topic->current); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.print(topic->charge); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.print(topic->capacity); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.print(topic->design_capacity); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.println(topic->percentage);
-      DEBUG_SERIAL.print(" Info(status,health,technology,present): ");
-      DEBUG_SERIAL.print(topic->power_supply_status); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.print(topic->power_supply_health); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.print(topic->power_supply_technology); DEBUG_SERIAL.print(","); 
-      DEBUG_SERIAL.println(topic->present);
-      DEBUG_SERIAL.print(" CellInfo(cell voltage:size): ");
-      for(uint32_t i = 0; i < topic->cell_voltage_size; i++)
-      {
-        DEBUG_SERIAL.print(topic->cell_voltage[i]); DEBUG_SERIAL.print(" "); 
-      }
-      DEBUG_SERIAL.print(": "); 
-      DEBUG_SERIAL.println(topic->cell_voltage_size);
-      DEBUG_SERIAL.print(" Location: ");
-      DEBUG_SERIAL.println(topic->location);
-      DEBUG_SERIAL.print(" Serial Number ");
-      DEBUG_SERIAL.println(topic->serial_number);
-
-      subscriber_->subscribe(STREAMID_BUILTIN_RELIABLE);
-    }
-  }
-
-
   ros2::Publisher<sensor_msgs::BatteryState>* publisher_;
   ros2::Subscriber<sensor_msgs::BatteryState>* subscriber_;
-
-  float cell_voltage_data_[3];
-  uint64_t nano_time_;
 };
 
 
 void setup() 
 {
-  DEBUG_SERIAL.begin(115200);
+  DEBUG_SERIAL.begin(57600);
   pinMode(LED_BUILTIN, OUTPUT);
   
   while (!RTPS_SERIAL);
@@ -138,4 +59,67 @@ void loop()
   }
 
   ros2::spin(&BatteryStateNode);
+}
+
+void publishBatteryState(void* topic_msg)
+{
+      sensor_msgs::BatteryState* msg = (sensor_msgs::BatteryState*)topic_msg;
+      static float cell_voltage_data_[3];
+
+      msg->header.frame_id          = (char*) "OpenCR BatteryState";
+      msg->header.stamp             = ros2::now();
+
+      msg->voltage                  = 1;
+      msg->current                  = 2;
+      msg->charge                   = 3;
+      msg->capacity                 = 4;
+      msg->design_capacity          = 5;
+      msg->percentage               = 6;
+
+      msg->power_supply_status      = 0;
+      msg->power_supply_health      = 1;
+      msg->power_supply_technology  = 2;
+
+      msg->present                  = true;
+      msg->cell_voltage             = cell_voltage_data_;
+      msg->cell_voltage_size        = sizeof(cell_voltage_data_)/sizeof(float);
+      for(uint32_t i = 0; i < msg->cell_voltage_size; i++)
+      {
+        msg->cell_voltage[i]        = 3.7;
+      }
+
+      msg->location                 = (char*)"Seoul";
+      msg->serial_number            = (char*)"123-456-789";
+}
+
+
+void subscribeBatteryState(void* topic_msg)
+{
+  sensor_msgs::BatteryState* msg = (sensor_msgs::BatteryState*)topic_msg;
+
+  DEBUG_SERIAL.println();
+  DEBUG_SERIAL.print(" Header(frameID,sec,nanosec): "); 
+    DEBUG_SERIAL.print(msg->header.frame_id); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.print(msg->header.stamp.sec); DEBUG_SERIAL.print("."); 
+    DEBUG_SERIAL.println(msg->header.stamp.nanosec);
+  DEBUG_SERIAL.print(" Value(voltage,current,charge,capacity,design capacity,percentage): ");
+    DEBUG_SERIAL.print(msg->voltage); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.print(msg->current); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.print(msg->charge); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.print(msg->capacity); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.print(msg->design_capacity); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.println(msg->percentage);
+  DEBUG_SERIAL.print(" Info(status,health,technology,present): ");
+    DEBUG_SERIAL.print(msg->power_supply_status); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.print(msg->power_supply_health); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.print(msg->power_supply_technology); DEBUG_SERIAL.print(","); 
+    DEBUG_SERIAL.println(msg->present);
+  DEBUG_SERIAL.print(" CellInfo(cell voltage:size): ");
+  for(uint32_t i = 0; i < msg->cell_voltage_size; i++)
+  {
+    DEBUG_SERIAL.print(msg->cell_voltage[i]); DEBUG_SERIAL.print(" "); 
+  }
+  DEBUG_SERIAL.print(": "); DEBUG_SERIAL.println(msg->cell_voltage_size);
+  DEBUG_SERIAL.print(" Location: "); DEBUG_SERIAL.println(msg->location);
+  DEBUG_SERIAL.print(" Serial Number "); DEBUG_SERIAL.println(msg->serial_number);
 }
