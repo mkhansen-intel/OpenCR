2929b007bc5b0a8ef9d96d180d7d3f16295aa8e7
Kei <kkw@robotis.com>
2018-07-20 21:13:06 +0900

    Added turtlebot3 burger node for test. (Debugging now)

---
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3_ros2/examples/turtlebot3_burger/turtlebot3_core/turtlebot3_core_config.h
@@ -0,0 +1,261 @@
+/*******************************************************************************
+* Copyright 2016 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/* Authors: Yoonseok Pyo, Leon Jung, Darby Lim, HanCheol Cho, Gilbert, Kei Ki */
+
+#ifndef TURTLEBOT3_CORE_CONFIG_H_
+#define TURTLEBOT3_CORE_CONFIG_H_
+
+#include <ros2.hpp>
+#include <std_msgs/Bool.hpp>
+#include <std_msgs/Empty.hpp>
+#include <std_msgs/Int32.hpp>
+#include <sensor_msgs/Imu.hpp>
+#include <sensor_msgs/JointState.hpp>
+#include <sensor_msgs/BatteryState.hpp>
+#include <sensor_msgs/MagneticField.hpp>
+#include <geometry_msgs/Vector3.hpp>
+#include <geometry_msgs/Twist.hpp>
+//#include <tf/tf.hpp>
+//#include <tf/transform_broadcaster.hpp>
+#include <nav_msgs/Odometry.hpp>
+
+#include <turtlebot3_msgs/SensorState.hpp>
+#include <turtlebot3_msgs/Sound.hpp>
+#include <turtlebot3_msgs/VersionInfo.hpp>
+
+#include <TurtleBot3_ROS2.h>
+#include "turtlebot3_burger.h"
+
+#include <math.h>
+
+#define HARDWARE_VER "0.1.0"
+#define SOFTWARE_VER "0.1.0"
+#define FIRMWARE_VER "0.1.0"
+
+#define SENSOR_STATE_PUBLISH_FREQUENCY         30    //hz
+#define VERSION_INFORMATION_PUBLISH_FREQUENCY  1    //hz 
+#define IMU_PUBLISH_FREQUENCY                  200  //hz
+#define CMD_VEL_PUBLISH_FREQUENCY              30   //hz
+#define ODOMETRY_PUBLISH_FREQUENCY             30  //hz
+#define JOINT_STATE_PUBLISH_FREQUENCY          30   //hz
+#define BATTERY_STATE_PUBLISH_FREQUENCY        30   //hz
+#define MAGNETIC_FIELD_PUBLISH_FREQUENCY       30   //hz
+
+#define CONTROL_MOTOR_SPEED_FREQUENCY          30   //hz
+#define DEBUG_LOG_FREQUENCY                    10   //hz
+ 
+#define WHEEL_NUM                        2
+
+#define LEFT                             0
+#define RIGHT                            1
+
+#define LINEAR                           0
+#define ANGULAR                          1
+
+#define DEG2RAD(x)                       (x * 0.01745329252)  // *PI/180
+#define RAD2DEG(x)                       (x * 57.2957795131)  // *180/PI
+
+#define TICK2RAD                         0.001533981  // 0.087890625[deg] * 3.14159265359 / 180 = 0.001533981f
+
+#define TEST_DISTANCE                    0.300     // meter
+#define TEST_RADIAN                      3.14      // 180 degree
+
+//#define DEBUG                            
+#define DEBUG_SERIAL                     SerialBT2
+
+
+
+
+/*******************************************************************************
+* Declaration for motor
+*******************************************************************************/
+Turtlebot3MotorDriver motor_driver;
+
+/*******************************************************************************
+* Declaration for sensors
+*******************************************************************************/
+Turtlebot3Sensor sensors;
+
+/*******************************************************************************
+* Declaration for diagnosis
+*******************************************************************************/
+Turtlebot3Diagnosis diagnosis;
+
+/*******************************************************************************
+* Declaration for controllers
+*******************************************************************************/
+Turtlebot3Controller controllers;
+float goal_velocity[WHEEL_NUM] = {0.0, 0.0};
+float goal_velocity_from_button[WHEEL_NUM] = {0.0, 0.0};
+float goal_velocity_from_cmd[WHEEL_NUM] = {0.0, 0.0};
+float goal_velocity_from_rc100[WHEEL_NUM] = {0.0, 0.0};
+
+/*******************************************************************************
+* SoftwareTimer of Turtlebot3
+*******************************************************************************/
+static uint32_t tTime[10];
+
+/*******************************************************************************
+* Calculation for odometry
+*******************************************************************************/
+bool init_encoder = true;
+int32_t last_diff_tick[WHEEL_NUM] = {0, 0};
+double  joint_states_pos[WHEEL_NUM]  = {0.0, 0.0};
+
+/*******************************************************************************
+* Update Joint State
+*******************************************************************************/
+double  joint_states_vel[WHEEL_NUM]  = {0.0, 0.0};
+
+/*******************************************************************************
+* Declaration for SLAM and navigation
+*******************************************************************************/
+unsigned long prev_update_time;
+float odom_pose[3];
+double odom_vel[3];
+
+/*******************************************************************************
+* Declaration for Battery
+*******************************************************************************/
+bool setup_end        = false;
+uint8_t battery_state = 0;
+
+/*******************************************************************************
+* ROS Parameter
+*******************************************************************************/
+char get_prefix[10];
+char* get_tf_prefix = get_prefix;
+
+char odom_header_frame_id[30];
+char odom_child_frame_id[30];
+
+char imu_frame_id[30];
+char mag_frame_id[30];
+
+char joint_state_header_frame_id[30];
+
+
+void updateTFPrefix(bool isConnected);
+void updateVariable(bool isConnected);
+void updateGyroCali(bool isConnected);
+void updateGoalVelocity(void);
+void updateMotorInfo(int32_t left_tick, int32_t right_tick);
+bool calcOdometry(double diff_time);
+void driveTest(uint8_t buttons);
+void sendLogMsg(void);
+void sendDebuglog(void);
+
+
+void publishImu(void* topic_msg);
+void publishCmdVelRC100(void* topic_msg);
+void publishOdometry(void* topic_msg);
+void publishJointState(void* topic_msg);
+void publishBatteryState(void* topic_msg);
+void publishMagneticField(void* topic_msg);
+void publishSensorState(void* topic_msg);
+void publishVersionInfo(void* topic_msg);
+
+
+void subscribeCmdVel(void* topic_msg);
+void subscribeSound(void* topic_msg);
+void subscribeMotorPower(void* topic_msg);
+void subscribeReset(void* topic_msg);
+
+
+/*******************************************************************************
+* TurtleBot3 Node Class
+*******************************************************************************/
+class TurtleBot3 : public ros2::Node
+{
+public:
+  TurtleBot3()
+  : Node()
+  {
+    /*******************************************************************************
+    * Publisher
+    *******************************************************************************/
+    // IMU of Turtlebot3
+    imu_pub_           = this->createPublisher<sensor_msgs::Imu>("imu");
+    this->createWallFreq(IMU_PUBLISH_FREQUENCY, publishImu, imu_pub_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /imu            : "); DEBUG_SERIAL.println((imu_pub_!=NULL?"Success":"Fail"));
+    // Command velocity of Turtlebot3 using RC100 remote controller
+    cmd_vel_rc100_pub_ = this->createPublisher<geometry_msgs::Twist>("cmd_vel_rc100");
+    this->createWallFreq(CMD_VEL_PUBLISH_FREQUENCY, publishCmdVelRC100, cmd_vel_rc100_pub_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /cmd_vel_rc100  : "); DEBUG_SERIAL.println((cmd_vel_rc100_pub_!=NULL?"Success":"Fail"));
+    // Odometry of Turtlebot3
+    odom_pub_          = this->createPublisher<nav_msgs::Odometry>("odom");
+    this->createWallFreq(ODOMETRY_PUBLISH_FREQUENCY, publishOdometry, odom_pub_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /odom           : "); DEBUG_SERIAL.println((odom_pub_!=NULL?"Success":"Fail"));
+    // Joint(Dynamixel) state of Turtlebot3
+    joint_states_pub_  = this->createPublisher<sensor_msgs::JointState>("joint_states");
+    this->createWallFreq(JOINT_STATE_PUBLISH_FREQUENCY, publishJointState, joint_states_pub_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /joint_states   : "); DEBUG_SERIAL.println((joint_states_pub_!=NULL?"Success":"Fail"));
+    // Battey state of Turtlebot3
+    battery_state_pub_ = this->createPublisher<sensor_msgs::BatteryState>("battery_state");
+    this->createWallFreq(BATTERY_STATE_PUBLISH_FREQUENCY, publishBatteryState, battery_state_pub_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /battery_state  : "); DEBUG_SERIAL.println((battery_state_pub_!=NULL?"Success":"Fail"));
+    // Magnetic field
+    mag_pub_           = this->createPublisher<sensor_msgs::MagneticField>("magnetic_field");
+    this->createWallFreq(MAGNETIC_FIELD_PUBLISH_FREQUENCY, publishMagneticField, mag_pub_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /magnetic_field : "); DEBUG_SERIAL.println((mag_pub_!=NULL?"Success":"Fail"));
+    // Sensor State
+    sensor_state_pub_  = this->createPublisher<turtlebot3_msgs::SensorState>("sensor_state");
+    this->createWallFreq(SENSOR_STATE_PUBLISH_FREQUENCY, publishSensorState, sensor_state_pub_);  
+    DEBUG_SERIAL.print(" [Publisher Create]   /sensor_state   : "); DEBUG_SERIAL.println((sensor_state_pub_!=NULL?"Success":"Fail"));
+    // Version information of Turtlebot3
+    version_info_pub_  = this->createPublisher<turtlebot3_msgs::VersionInfo>("version_info");
+    this->createWallFreq(VERSION_INFORMATION_PUBLISH_FREQUENCY, publishVersionInfo, version_info_pub_);
+    DEBUG_SERIAL.print(" [Publisher Create]   /version_info   : "); DEBUG_SERIAL.println((version_info_pub_!=NULL?"Success":"Fail"));
+
+   
+    /*******************************************************************************
+    * Subscriber
+    *******************************************************************************/
+    cmd_vel_sub_       = this->createSubscriber<geometry_msgs::Twist>("cmd_vel", subscribeCmdVel);
+    DEBUG_SERIAL.print(" [Subscriber Create]  /cmd_vel        : "); DEBUG_SERIAL.println((cmd_vel_sub_!=NULL?"Success":"Fail"));
+    sound_sub_         = this->createSubscriber<turtlebot3_msgs::Sound>("sound", subscribeSound);
+    DEBUG_SERIAL.print(" [Subscriber Create]  /sound          : "); DEBUG_SERIAL.println((sound_sub_!=NULL?"Success":"Fail"));
+    motor_power_sub_   = this->createSubscriber<std_msgs::Bool>("motor_power", subscribeMotorPower);
+    DEBUG_SERIAL.print(" [Subscriber Create]  /motor_power    : "); DEBUG_SERIAL.println((motor_power_sub_!=NULL?"Success":"Fail"));
+    reset_sub_         = this->createSubscriber<std_msgs::Empty>("reset", subscribeReset);
+    DEBUG_SERIAL.print(" [Subscriber Create]  /reset          : "); DEBUG_SERIAL.println((reset_sub_!=NULL?"Success":"Fail"));
+  }
+
+
+private:
+
+  /* Publisher Pointer */
+  ros2::Publisher<turtlebot3_msgs::SensorState>*  sensor_state_pub_;
+  ros2::Publisher<turtlebot3_msgs::VersionInfo>*  version_info_pub_;
+  ros2::Publisher<sensor_msgs::Imu>*              imu_pub_;
+  ros2::Publisher<geometry_msgs::Twist>*          cmd_vel_rc100_pub_;
+  ros2::Publisher<nav_msgs::Odometry>*            odom_pub_;
+  ros2::Publisher<sensor_msgs::JointState>*       joint_states_pub_;
+  ros2::Publisher<sensor_msgs::BatteryState>*     battery_state_pub_;
+  ros2::Publisher<sensor_msgs::MagneticField>*    mag_pub_;
+
+  /* Subscriber Pointer */
+  ros2::Subscriber<geometry_msgs::Twist>*         cmd_vel_sub_;
+  ros2::Subscriber<turtlebot3_msgs::Sound>*       sound_sub_;
+  ros2::Subscriber<std_msgs::Bool>*               motor_power_sub_;
+  ros2::Subscriber<std_msgs::Empty>*              reset_sub_;
+};
+
+
+
+#endif // TURTLEBOT3_CORE_CONFIG_H_
+
